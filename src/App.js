a6cf72eaa683db216b56ebaf97c2fbc6a7d51db7
Mishra-raja <rajamishra0702@gmail.com>
2021-02-08 22:40:28 +0530

    added Navbar and custom styles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react'
-import Products from './components/Products/Products'
+// import Products from './components/Products/Products'
+// import Navbar from "./components/Navbar/Navbar";
+
+import { Products, Navbar } from "./components";
 
 const App = () => {
     return (
-        <div>
-           <Products/>
-        </div>
-    )
+      <div>
+        <Navbar />
+        <Products />
+      </div>
+    );
 }
 
 export default App
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -9,8 +9,36 @@ import {
   Typography,
 } from "@material-ui/core";
 import { ShoppingCart } from "@material-ui/icons";
+import logo from "../../assets/commerce.png";
+import useStyles from "./styles";
+
 const Navbar = () => {
-  return <div></div>;
+  const classes = useStyles();
+  return (
+    <>
+      <AppBar position="fixed" className={classes.appBar} color="inherit">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title} color="inherit">
+            <img
+              src={logo}
+              alt="react-commerce"
+              height="25px"
+              className={classes.image}
+            />
+            react-commerce
+          </Typography>
+          <div className={classes.grow} />
+          <div className={classes.button}>
+            <IconButton aria-label="Show cart items" color="inherit">
+              <Badge badgeContent={2} color="Secondary">
+                <ShoppingCart />
+              </Badge>
+            </IconButton>
+          </div>
+        </Toolbar>
+      </AppBar>
+    </>
+  );
 };
 
 export default Navbar;
